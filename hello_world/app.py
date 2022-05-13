d63cf2e4e22dd887624510df26ebf9e47102e04a
Daniel Mil <mladan@amazon.com>
2022-05-12 18:18:31 -0700

    Update permissions

--- hello_world/app.py
+++ hello_world/app.py
@@ -4,11 +4,16 @@ import requests
 
 def get_appconfig_data(application_name, environment_name, configuration_name):
     url = f'http://localhost:2772/applications/{application_name}/environments/{environment_name}/configurations/{configuration_name}'
-    config = requests.get(url)
+    print(f"Making request to: {url}")
+    config = requests.get(url).json()
     return config
 
 def lambda_handler(event, context):
     application_name = os.getenv("APPCONFIG_APPLICATION")
     environment_name = os.getenv("APPCONFIG_ENVIRONMENT")
     configuration_name = os.getenv("APPCONFIG_CONFIGURATION")
-    return json.dumps(get_appconfig_data(application_name, environment_name, configuration_name))
+    print(f"{application_name}\n{environment_name}\n{configuration_name}")
+    return {
+        "statusCode": 200,
+        "body": json.dumps(get_appconfig_data(application_name, environment_name, configuration_name))
+    }
